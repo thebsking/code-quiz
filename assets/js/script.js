6a6b0ea8ff833ee82cc6c2d664c232bc0cbe0e29
Brandon King <brandonscottking@gmail.com>
2021-02-15 20:21:46 -0500

    js variables declared

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,50 @@
-var timer = document.querySelector("#timer");
-var startBtn = document.querySelector("#start");
-var questionArea = document.querySelector("#question");
-var selectionArea = document.querySelector("#options");
-
-let testQuestions = [
+//declare variables
+let timer = document.querySelector("#timer");
+let startBtn = document.querySelector("#start");
+let questionArea = document.querySelector("#question");
+let selectionArea = document.querySelector("#options");
+let userScore = 0;
+let testContent = [
     {
-        question: "What does CSS stand for?",
-        answers:{
-            a: "Cascading Style Sheet",
-            b: "Computer Style Sheet",
-            c: "Computers Stupid Spelling",
-            d: "Crunch Super Soap"
-    }},
-    {
-        question: "",
-        a: "",
-        b: "",
-        c: "",
-        d: ""
+        question: '',
+        options: [''],
+        answer: ''
     },
     {
-        question: "",
-        a: "",
-        b: "",
-        c: "",
-        d: ""
+        question: '',
+        options: [''],
+        answer: ''
     },
     {
-        question: "",
-        a: "",
-        b: "",
-        c: "",
-        d: ""
+        question: '',
+        options: [''],
+        answer: ''
     },
     {
-        question: "",
-        a: "",
-        b: "",
-        c: "",
-        d: ""
+        question: '',
+        options: [''],
+        answer: ''
+    },
+    {
+        question: '',
+        options: [''],
+        answer: ''
     },
 ];
 
+//countdown timer
+function countdown() {
+    let timeLeft = 60;
+  
+    // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+    let timeInterval = setInterval(function() {
+      timeLeft--;
+      timer.textContent = `${timeLeft} seconds remaining`;
+  
+      if (timeLeft === 0) {
+      clearInterval(timeInterval);
+      timer.textContent = "Time's up!";
+    }}, 1000)
+    
+  };
 
-
-function startGame() {
-    for (var i = 0; i < testQuestions.length; i++) {
-        questionArea.textContent = testQuestions[i].question;
-        selectionArea.textContent = testQuestions[i].a;
-    }; 
-};
