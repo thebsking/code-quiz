61c4f18b3a14390d55e599014fb46a4411979524
Brandon King <brandonscottking@gmail.com>
2021-02-19 23:02:29 -0500

    add event listeners in scores.js & updated css rules

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -2,9 +2,14 @@
 let scoreString = window.localStorage.getItem(("scores"));
 let scoreArray = JSON.parse(scoreString);
 let scoreList = document.querySelector("#scores-list")
-
+let mainEl = document.querySelector("main")
 
 //display scores
+if (!scoreArray) {
+    let noScores = document.createElement("h2");
+    noScores.textContent = "No Scores Yet";  
+    mainEl.appendChild(noScores);
+} else {
 for (var i = 0; i < scoreArray.length; i++) {
     let listName = document.createElement("li");
     let listScore = document.createElement("li");
@@ -12,4 +17,23 @@ for (var i = 0; i < scoreArray.length; i++) {
     scoreList.appendChild(listName);
     listScore.textContent = scoreArray[i].score;
     scoreList.appendChild(listScore);
-}
+}}
+
+//create buttons to clear scores & play again 
+let clearButton = document.createElement("button");
+let playButton = document.createElement("button");
+clearButton.textContent = "Clear All Scores";
+playButton.textContent = "Play Again";
+
+//append buttons to page
+mainEl.appendChild(clearButton);
+mainEl.appendChild(playButton);
+
+//event listeners for buttons
+clearButton.addEventListener("click", function (event){
+    window.localStorage.clear();
+    window.location.reload();
+})
+playButton.addEventListener("click", function(event){
+    window.location.href = "./index.html"
+})
